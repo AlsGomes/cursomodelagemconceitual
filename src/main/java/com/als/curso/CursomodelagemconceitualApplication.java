3f7a970b8ba136c9b6af72ef50df6ffb2370b1fc
Alisson Gomes <44034024+AlsGomes@users.noreply.github.com>
2020-09-12 00:49:58 -0300

    Criando operacao de instanciacao

--- src/main/java/com/als/curso/CursomodelagemconceitualApplication.java
+++ src/main/java/com/als/curso/CursomodelagemconceitualApplication.java
@@ -1,13 +1,31 @@
 package com.als.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.als.curso.domain.Categoria;
+import com.als.curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomodelagemconceitualApplication {
+public class CursomodelagemconceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodelagemconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
