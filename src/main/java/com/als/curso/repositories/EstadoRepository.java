00fd590c7a95fd956c40af3d3726efed660709ba
Alisson Gomes <44034024+AlsGomes@users.noreply.github.com>
2020-09-13 17:19:43 -0300

    Estado e Cidade

---
+++ src/main/java/com/als/curso/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.als.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.als.curso.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
